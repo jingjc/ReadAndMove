c78f15d8560dd24e1609de22e2c3347853d0a7f8
jingjc <1432403402@qq.com>
2018-05-23 17:55:17 +0800

    第二次提交

--- pages/post/post_details/post_details.js
+++ pages/post/post_details/post_details.js
@@ -1,30 +1,20 @@
-// pages/post/post.js
-var postData=require('../../data/posts-data.js')
+// pages/post/post_details.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      posts_key: postData.postList
-    });
+  
   },
 
-  onclickitem:function(event){
-    var postid=event.currentTarget.dataset.postid;
-    console.log(postid);
-      wx.navigateTo({
-        url: 'post_details/post_details',
-      })
-  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
