7554540964ad2f56dce15f723b87be8ce1b225bb
jingjc <1432403402@qq.com>
2018-06-14 18:25:05 +0800

    20180614

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -9,11 +9,8 @@ Page({
   },
 
   click:function(){
-   wx.redirectTo({
+   wx.switchTab({
      url: '/pages/post/post',
-     success: function(res) {},
-     fail: function(res) {},
-     complete: function(res) {},
    })
   },
 
@@ -23,53 +20,4 @@ Page({
   onLoad: function (options) {
   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
